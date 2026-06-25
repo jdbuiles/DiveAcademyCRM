dda6b5c534799a4004793c4792ef154d8432bb31
jdbuiles <diegobuiles1983@gmail.com>
2026-06-25 17:41:58 -0500

    Delete dive-center-backend directory

--- dive-center-backend/src/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-// Aplicar restricción estricta de Administrador a todas las rutas de este archivo
-router.use(verificarRol(['Administrador']));
-router.get('/', dashboardController.obtenerDashboardGlobal);
-
-module.exports = router;
--- dive-center-backend/src/routes/transaccionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const transaccionController = require('../controllers/transaccionController');
-const verificarRol = require('../middleware/authMiddleware');
-
-// Solo los Administradores pueden registrar flujos de caja de ingresos/egresos
-router.post('/', verificarRol(['Administrador']), transaccionController.crearTransaccion);
-
-module.exports = router;
