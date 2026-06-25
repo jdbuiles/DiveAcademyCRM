24d8f729b9e4ade4be23746609e64a13ca350cfa
jdbuiles <diegobuiles1983@gmail.com>
2026-06-25 15:41:38 -0500

    Update transaccionRoutes.js

--- dive-center-backend/src/routes/transaccionRoutes.js
+++ dive-center-backend/src/routes/transaccionRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const transaccionController = require('../controllers/transaccionController');
+const verificarRol = require('../middleware/authMiddleware');
 
-router.route('/')
-  .post(transaccionController.crearTransaccion);
+// Solo los Administradores pueden registrar flujos de caja de ingresos/egresos
+router.post('/', verificarRol(['Administrador']), transaccionController.crearTransaccion);
 
 module.exports = router;
