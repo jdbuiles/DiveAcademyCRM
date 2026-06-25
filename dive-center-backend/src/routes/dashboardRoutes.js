0095a7f87301da82e7a00d6cde74d352246d0157
jdbuiles <diegobuiles1983@gmail.com>
2026-06-25 15:40:17 -0500

    Update dashboardRoutes.js

--- dive-center-backend/src/routes/dashboardRoutes.js
+++ dive-center-backend/src/routes/dashboardRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/dashboardController');
-
+// Aplicar restricción estricta de Administrador a todas las rutas de este archivo
+router.use(verificarRol(['Administrador']));
 router.get('/', dashboardController.obtenerDashboardGlobal);
 
 module.exports = router;
